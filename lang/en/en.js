d7b92c632c9a422c9a14a36a1f09fe9aa4bfbb86
izcheung <icheung24@my.bcit.ca>
2025-10-14 18:01:10 -0700

    feat: add user facing messages

---
+++ lang/en/en.js
@@ -0,0 +1,7 @@
+export const messages = {
+  firstButtonText: "Click here to insert data",
+  secondButtonText: "Submit",
+  emptyQuery: "Please enter a SQL query",
+  textAreaPlaceholder: "Enter a SQL query",
+  unknownSQLCommand: "Unknown SQL command",
+};
