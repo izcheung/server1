e2467d08a7596f2dd41faa402d9b9e4433545b5f
izcheung <icheung24@my.bcit.ca>
2025-10-19 07:21:38 -0600

    fix: put local var separate line

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // ChatGPT was used to double check the code and find places to refactor
 import { messages } from "../lang/en/en.js";
 
-const SERVER_URL = "https://comp4537lab5-6d0f.onrender.com"; //'http://localhost:8001
+const SERVER_URL = "https://comp4537lab5-6d0f.onrender.com";
+// const SERVER_URL = 'http://localhost:8001"
 
 class SqlQuery {
   async handleGet(endpoint, responseElement) {
