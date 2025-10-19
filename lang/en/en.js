cce67f22f7c7abedb5e6966b9f03f55c715ed4f9
izcheung <icheung24@my.bcit.ca>
2025-10-19 06:43:56 -0600

    docs: add comments

--- lang/en/en.js
+++ lang/en/en.js
@@ -1,7 +1,20 @@
 export const messages = {
-  firstButtonText: "Click here to insert data",
-  secondButtonText: "Submit",
+  // Defined Query messages
+  definedQueryDescription:
+    "This button sends fixed patient records via POST request to /insert-data",
+  definedQueryButtonText: "Insert defined query",
+
+  // Custom Query messages
+  textareaPurpose:
+    "Enter a SQL query. A SELECT query sends via GET. An INSERT query sends via POST.",
+  customQueryButtonText: "Submit",
+
+  textAreaPlaceholder:
+    "Ex: SELECT * FROM patients WHERE dateOfBirth > 30; Ex: INSERT INTO patients (name, dateOfBirth) VALUES ('Another Patient', '2026-01-01');",
+
+  // Server response messages
   emptyQuery: "Please enter a SQL query",
-  textAreaPlaceholder: "Enter a SQL query",
-  unknownSQLCommand: "Unknown SQL command",
+  unsupportedSQLQuery: "Only SELECT and INSERT queries allowed",
+  requestFailed: `Request failed with status %1: %2`,
+  serverError: `Network or Server Error:: %1`,
 };
